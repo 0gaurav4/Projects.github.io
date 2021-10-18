8977b03e4b2c93c6b6ed77772e631756f4557c79
mysgrown <76728138+0gaurav4@users.noreply.github.com>
2021-10-18 08:58:28 +0530

    Update jarvis.py

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -1,10 +1,15 @@
-import pyttsx3
-import wikipedia
-import speech_recognition as sr
+import pyttsx3  #pip install pyttsx3
+import wikipedia  #pip install wikipedia
+import speech_recognition as sr   #pip install SpeechRecognition
 import datetime
 import webbrowser
 import os
+import random
+import cv2  #pip install opencv-python
 import smtplib
+from requests import get
+import pywhatkit
+import sys
 
 
 engine = pyttsx3.init('sapi5')
@@ -12,29 +17,36 @@ voices = engine.getProperty('voices')
 #print(voices[0].id)
 engine.setProperty('voice', voices[0].id)
 
+#text to speech
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+#to wish
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
         speak("Good Morning!")
+        print("Good Morning!")
 
     elif hour>=12 and hour<18:
         speak("Good Afternoon!")
+        print("Good Afternoon!")
 
     elif hour>=18 and hour<22:
         speak("Good Evening!")
+        print("Good Evening!")
 
     else:
         speak("Good Night!")
+        print("Good Night!")
 
-    speak("I am your voice assistant. Please tell me how may i help you")
+    print("I am your 'desktop voice assistant'. Please tell me how can i help you...")
+    speak("I am your 'desktop voice assistant'. Please tell me how can i help you...")
 
+#it takes microphone input from the user and returns string output
 def takeCommand():
-    #it takes microphone input from the usser and returns string output
-
+   
     r= sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
@@ -54,11 +66,12 @@ def takeCommand():
         return "None"
     return query
 
+#send email
 def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('gaurav.tiwaribca2019@bbdu.ac.in' '#shreya$honeY1*')
+    server.login('gaurav.tiwaribca2019@bbdu.ac.in' 'your-password')
     server.sendmail('gaurav.tiwaribca2019@bbdu.ac.in', to, content)
     server.close()
     
@@ -68,6 +81,8 @@ if __name__ == "__main__":
     wishMe() 
     while True:
        query = takeCommand().lower()
+
+
        # Logic for executing tasks based on query
        if 'wikipedia' in query:
            speak('Searching To Wikipedia...')
@@ -89,14 +104,24 @@ if __name__ == "__main__":
        elif 'google' in query:
            speak('What should i search on Google...')
            cm =takeCommand().lower()
-           webbrowser.open(f"{cm}")
+           webbrowser.open(f"https://www.google.co.in/search?q={cm}&sxsrf=AOaemvIzUgQknzeXrGKHBntUxPzyKPI3xQ%3A1634490340803&source=hp&ei=5FdsYbDILcOB-QaU5YCwAw&iflsig=ALs-wAMAAAAAYWxl9HZVjrAxwKov-34XCteU914Lvp9l&ved=0ahUKEwiwypiB99HzAhXDQN4KHZQyADYQ4dUDCAg&oq=se&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAgjECcyBAgjECcyBwgAELEDEEMyBAgAEEMyBQgAEIAEMggIABCABBCxAzIFCC4QgAQyBQgAEIAEMggILhCABBCxAzoHCCMQ6gIQJzoLCAAQgAQQsQMQgwE6CAguELEDEIMBOhEILhCABBCxAxCDARDHARDRA1C-sgJY8LMCYOq8AmgDcAB4AoAB-QmIAcYVkgELMi0xLjEuNi0xLjGYAQCgAQGwAQo&sclient=gws-wiz&uact=5")
            
         
        elif 'open music' in query:
            music_dir = 'C:\\Users\\gaura\\Music'
            songs = os.listdir(music_dir)
-           print(songs)
-           os.startfile(os.path.join(music_dir, songs[0]))
+           #rd = random.choice(songs)
+           for song in songs:
+               if song.endswith('.mp3'):
+                   print(songs)
+           os.startfile(os.path.join(music_dir, song))
+
+
+       elif 'ip address' in query:
+           ip=get('https://api.ipify.org').text
+           print(f"your IP address is {ip}")
+           speak(f"your IP address is {ip}")
+           
 
        elif 'the time' in query:
            strTime = datetime.datetime.now().strftime("%H:%M:%S")
@@ -107,10 +132,28 @@ if __name__ == "__main__":
            codePath ="C:\\Users\\gaura\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
            os.startfile(codePath)
 
+       elif 'open notepad' in query:
+           nPath ="C:\Windows\System32\\notepad.exe"
+           os.startfile(nPath)
+
+       elif 'open command' in query:
+           os.system("start cmd")
+
+       elif 'open camera' in query:
+           cap = cv2.VideoCapture(0)
+           while True:
+               ret, img = cap.read()
+               cv2.imshow('webcam', img)
+               k = cv2.waitkey(50)
+               if k==27:
+                   break;
+           cap.release()
+           cv2.destroyAllWindows()
+
        elif 'email to Gaurav' in query:
            try:
                speak("What should i say?")
-               content = takeCommand()
+               content = takeCommand().lower()
                to = "gaurav.tiwaribca2019@bbdu.ac.in"
                sendEmail(to, content)
                speak("Email has been sent!")
@@ -118,8 +161,15 @@ if __name__ == "__main__":
                print(e)
                speak("Sorry for inconvinience. I am unable to send this email")
 
-       elif 'exit' in query:
-           exit()
+       elif 'send message' in query:
+           pywhatkit.sendwhatmsg("+9170884817", "this is testing protocol",2,25)
+
+       #to exit
+       elif 'no thanks' in query:
+           speak("Thanks For using me, and have a good day!")
+           sys.exit()
+
+       speak("Do you have any other work!...")
                
 
 
